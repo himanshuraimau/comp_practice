59f3793616b8335cd4ca7f6895e20a95ebac9378
Himanshu Rai <himanshuraimau9@gmail.com>
2024-06-09 22:11:35 +0530

    add the structure

---
+++ infinite_comments/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Comment from './components/Comment'
+import './App.css'
+
+function App() {
+
+
+  return (
+     <div>
+ 
+     </div>
+  )
+}
+
+export default App
